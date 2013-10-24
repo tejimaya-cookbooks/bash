22ef81ae38c59b51d86e7ac291f52e9f099a90ab
yoshino takahiro <yoshino@tejimaya.com>
2013-10-24 16:34:27 +0900

    rename README.rd -> README

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ maintainer       'Tejimaya.inc'
 maintainer_email 'support@tejimaya.com'
 license          'GPL v3'
 description      'Installs/Configures bash'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.rd'))
+long_description IO.read(File.join(File.dirname(__FILE__), 'README'))
 version          '0.1.0'
 
 recipe "bash", "Installs bash"
