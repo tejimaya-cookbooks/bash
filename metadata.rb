6748037addbcadd576cd68a9bf5100e369c86be7
yoshino takahiro <yoshino@tejimaya.com>
2014-03-17 14:34:53 +0900

    bump up version

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ maintainer_email 'support@tejimaya.com'
 license          'GPL v3'
 description      'Installs/Configures bash'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README'))
-version          '0.1.1'
+version          '0.1.2'
 
 recipe "bash", "Installs bash"
