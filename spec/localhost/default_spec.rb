e2a90636ed40868c390b69bdf08561c892cb8962
yoshino takahiro <yoshino@tejimaya.com>
2014-06-30 15:29:41 +0900

    /bin/bash 確認のテストを追加

--- spec/localhost/default_spec.rb
+++ spec/localhost/default_spec.rb
@@ -4,6 +4,10 @@ describe package('bash') do
   it { should be_installed }
 end
 
+describe file("/bin/bash") do
+  it { should be_executable }
+end
+
 describe file("/etc/skel/.bash_login") do
   it { should be_file }
 
