5a4357f8e6235020e9102f0a8260726815be3a6c
yoshino takahiro <yoshino@tejimaya.com>
2014-06-30 15:29:19 +0900

    Fixes #1 ls color の設定を追加

--- spec/localhost/default_spec.rb
+++ spec/localhost/default_spec.rb
@@ -6,4 +6,8 @@ end
 
 describe file("/etc/skel/.bash_login") do
   it { should be_file }
+
+  # ls color
+  it { should contain "LS_OPTIONS=\"--color=auto\"" }
+  it { should contain "alias ls='ls $LS_OPTIONS'" }
 end
