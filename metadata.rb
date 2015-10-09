d9fe292a7a09b7905463c1b608ec7f838d7e8569
yoshino <yoshino@tejimaya.com>
2015-10-09 12:51:00 +0900

    bump up version : v0.1.5

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,6 @@ maintainer_email 'support@tejimaya.com'
 license          'GPL v3'
 description      'Installs/Configures bash'
 long_description "show README"
-version          '0.1.4'
+version          '0.1.5'
 
 recipe "bash", "Installs bash"
